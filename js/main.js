02769d980c6e5647d6cfe3eede24d44ffd58eead
David Kim <daviddarden11@gmail.com>
2013-11-30 20:12:13 -0800

    basics

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 var app = angular.module('myApp', []);
+
+app.run(function ($rootScope) {
+  $rootScope.name = "this is in the rootScope";
+});
+
+app.controller('ParentController', function ($scope) {
+  $scope.person = { greeted: false };
+});
+
+app.controller('ChildController', function ($scope) {
+  $scope.sayHello = function () {
+    $scope.person.greeted = true;
+  }
+  $scope.reset = function () {
+    $scope.person.greeted = false;
+  }
+});
