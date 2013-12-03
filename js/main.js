b35e298adb8ce2f56d7eb849e9eca550d50180bb
David Kim <daviddarden11@gmail.com>
2013-12-02 20:20:46 -0800

    audio with

--- js/main.js
+++ js/main.js
@@ -4,15 +4,25 @@ app.run(function ($rootScope) {
   $rootScope.name = "this is in the rootScope";
 });
 
-app.controller('ParentController', function ($scope) {
-  $scope.person = { greeted: false };
-});
+app.controller('PlayerController', ['$scope', function ($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src ='http://download.wavetlan.com/SVV/Media/HTTP/sample_300kbit.mp4'
+  $scope.play = function () {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function () {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function () {
+    $scope.$apply(function () {
+      $scope.stop()
+    });
+  });
+}]);
 
-app.controller('ChildController', function ($scope) {
-  $scope.sayHello = function () {
-    $scope.person.greeted = true;
-  }
-  $scope.reset = function () {
-    $scope.person.greeted = false;
-  }
-});
+app.controller('RelatedController', ['$scope', function ($scope) {
+
+}]);
